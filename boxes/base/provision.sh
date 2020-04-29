fa5cc2bdffa7a9011223969d2d82989ddf972773
Bruno Duarte <git@brunoduarte.net>
2020-04-29 15:33:43 +0300

    We add timestamp and git branch to the shell

--- boxes/base/provision.sh
+++ boxes/base/provision.sh
@@ -18,3 +18,10 @@ sudo apt-get install -y net-tools locate vim nano tcpdump dnsutils traceroute cu
 # Create the directory for mounting the VirtualBox Guest Additions
 # sudo mkdir /tmp/mount
 # sudo chmod 777 /tmp/mount
+
+# We add timestamp and git branch to the shell
+echo "parse_git_branch() {
+             git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+PS1='\033[1;33m(\t)\033[m \033[1;36m[ \u |\033[m \033[1;32m\W\033[m \033[1;36m]\033[m $(parse_git_branch)\n> '
+" | tee -a /root/.bashrc /home/vagrant/.bashrc

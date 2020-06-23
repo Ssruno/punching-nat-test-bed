b4add6f2043f848e775b2e07c458de0a620b18b8
Bruno Duarte <git@brunoduarte.net>
2020-06-23 18:21:22 +0300

    We add aes256gcm128-sha256-curve25519

--- benchmark-ipsec/provision/lighthouse1.sh
+++ benchmark-ipsec/provision/lighthouse1.sh
@@ -1,30 +1,10 @@
 #!/usr/bin/env bash
 
-# If binaries (package) of nebula is not present, download it
-# if [ ! -f "/vagrant/nebula-linux-amd64.tar.gz" ]
-# then
-#   wget -P /vagrant https://github.com/slackhq/nebula/releases/download/v1.2.0/nebula-linux-amd64.tar.gz
-# fi
-
-# Delete certificates, binaries, etc from previous Nebula deployment
-# if [ -n "$(ls -A /vagrant/files/ 2>/dev/null)" ]
-# then
-#   rm -rf /vagrant/files/*.key
-#   rm -rf /vagrant/files/*.crt
-#   rm -rf /vagrant/files/nebula
-#   rm -rf /vagrant/files/nebula-cert
-# fi
-
-# tar -xvf /vagrant/nebula-linux-amd64.tar.gz -C /vagrant/files
-# cd /vagrant/files/
-# /vagrant/files/nebula-cert ca -name "Andromeda Galaxy, Inc"
-
-# /vagrant/files/nebula-cert sign -name "lighthouse1" -ip "192.200.1.100/24"
-# /vagrant/files/nebula-cert sign -name "node-a1"     -ip "192.200.1.5/24" 
 
 # We generate key-pair for scp or rsync without passphrase
 # ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
 
+# Copy certificates
 cp /vagrant/config/pki/server-root-ca.pem   /etc/ipsec.d/cacerts/
 cp /vagrant/config/pki/lighthouse1-key.pem  /etc/ipsec.d/private/
 cp /vagrant/config/pki/lighthouse1-cert.pem /etc/ipsec.d/certs/
@@ -47,6 +27,9 @@ conn lighthouse1-to-node-a1
     auto=start    
     dpdaction=clear
     keyexchange=ikev2
+
+    ike=aes256gcm128-sha256-curve25519!
+    esp=aes256gcm128-sha256-curve25519!
 
     left=172.20.1.100
     leftid="C=FI, O=VPN lighthouse1, CN=172.20.1.100"
--- benchmark-ipsec/provision/node_a1.sh
+++ benchmark-ipsec/provision/node_a1.sh
@@ -2,19 +2,11 @@
 
 #sudo /vagrant/files/nebula -config /vagrant/config/node-a1/config.yml &
 
-# We generate files with random content.
-# head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
-# head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
-# head -c 50M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/03_origin_file_0050M.dat
-# head -c 100M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/04_origin_file_0100M.dat
-# head -c 500M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/05_origin_file_0500M.dat
-# head -c 1000M < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/06_origin_file_1000M.dat
-
 # We generate key-pair for scp or rsync without passphrase
 # ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
 
 # Manually run this
-# ssh-copy-id -i id_rsa.pub vagrant@192.200.1.100
+# ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@172.20.1.100
 
 
 cp /vagrant/config/pki/server-root-ca.pem   /etc/ipsec.d/cacerts/
@@ -34,6 +26,9 @@ conn node-a1-to-lighthouse1
     auto=start    
     dpdaction=clear
     keyexchange=ikev2
+
+    ike=aes256gcm128-sha256-curve25519!
+    esp=aes256gcm128-sha256-curve25519!
 
     right=172.20.1.100    
     rightid="C=FI, O=VPN lighthouse1, CN=172.20.1.100"

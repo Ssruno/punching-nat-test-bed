056a58087a2172fa315a9046aa807e882ff72668
Bruno Duarte <git@brunoduarte.net>
2020-06-25 20:45:39 +0300

    changes to 1500 mtu in experiment 01 nebula

--- benchmark-nebula/experiments/01_nebula_delay/origin/01_nebula_delay_experiment.sh
+++ benchmark-nebula/experiments/01_nebula_delay/origin/01_nebula_delay_experiment.sh
@@ -5,12 +5,12 @@
 #/usr/bin/time  -f "%e, 1024" scp /vagrant/benchmark/origin/01_origin_file_0001M.dat vagrant@172.20.1.100:/vagrant/benchmark/destination/ >> /vagrant/benchmark/results/delay.csv
 
 # We generate files with random content.
-head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
-head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
-head -c 50M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/03_origin_file_0050M.dat
-head -c 100M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/04_origin_file_0100M.dat
-head -c 500M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/05_origin_file_0500M.dat
-head -c 1000M < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/06_origin_file_1000M.dat
+# head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
+# head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
+# head -c 50M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/03_origin_file_0050M.dat
+# head -c 100M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/04_origin_file_0100M.dat
+# head -c 500M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/05_origin_file_0500M.dat
+# head -c 1000M < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/06_origin_file_1000M.dat
 
 destination="192.200.1.100"
 size_of_files=(1M 10M 50M 100M 500M 1000M)
@@ -21,7 +21,7 @@ name_of_files=(01_origin_file_0001M.dat 02_origin_file_0010M.dat 03_origin_file_
 
 #echo ${name_of_files[@]}
 
-echo "seconds;size;filename;iteration" > /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_results.csv
+echo "seconds;size;filename;iteration" > /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_1500_results.csv
 
 ## { /usr/bin/time  -f "%e, 1024" scp -q /vagrant/benchmark/origin/01_origin_file_0001M.dat vagrant@172.20.1.100:/vagrant/benchmark/destination/ ; } 2>> /vagrant/benchmark/results/delay.csv
 
@@ -30,6 +30,7 @@ for file in {0..5}
 do
     for iteration in {1..30}
     do        
-        { /usr/bin/time  -f "%e; ${size_of_files[$file]}; ${name_of_files[$file]}; $iteration" scp -q /vagrant/experiments/01_nebula_delay/${name_of_files[$file]} vagrant@$destination:/vagrant/benchmark/destination/ ; } 2>> /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_results.csv
+        echo "${size_of_files[$file]}; ${name_of_files[$file]}; $iteration"
+        { /usr/bin/time  -f "%e; ${size_of_files[$file]}; ${name_of_files[$file]}; $iteration" scp -q /vagrant/experiments/01_nebula_delay/origin/${name_of_files[$file]} vagrant@$destination:/vagrant/experiments/01_nebula_delay/destination/ ; } 2>> /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_1500_results.csv
     done
 done
--- benchmark-nebula/provision/node_a1.sh
+++ benchmark-nebula/provision/node_a1.sh
@@ -3,12 +3,12 @@
 #sudo /vagrant/files/nebula -config /vagrant/config/node-a1/config.yml &
 
 # We generate files with random content.
-head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
-head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
-head -c 50M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/03_origin_file_0050M.dat
-head -c 100M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/04_origin_file_0100M.dat
-head -c 500M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/05_origin_file_0500M.dat
-head -c 1000M < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/06_origin_file_1000M.dat
+# head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
+# head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
+# head -c 50M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/03_origin_file_0050M.dat
+# head -c 100M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/04_origin_file_0100M.dat
+# head -c 500M  < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/05_origin_file_0500M.dat
+# head -c 1000M < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/06_origin_file_1000M.dat
 
 # We generate key-pair for scp or rsync without passphrase
 # ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""

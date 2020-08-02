d85ffd24531ca7626df4b99b119368eaafca866d
Bruno Duarte <git@brunoduarte.net>
2020-08-02 12:40:07 +0300

    nebula scp no cap

--- benchmark-nebula/experiments/01_nebula_delay/origin/01_nebula_delay_experiment.sh
+++ benchmark-nebula/experiments/01_nebula_delay/origin/01_nebula_delay_experiment.sh
@@ -2,8 +2,6 @@
 
 #/usr/bin/time -o /vagrant/benchmark/results/delay.csv -f "%e, 1" scp /vagrant/benchmark/origin/01_origin_file_0001M.dat vagrant@172.20.1.100:/vagrant/benchmark/destination/
 
-#/usr/bin/time  -f "%e, 1024" scp /vagrant/benchmark/origin/01_origin_file_0001M.dat vagrant@172.20.1.100:/vagrant/benchmark/destination/ >> /vagrant/benchmark/results/delay.csv
-
 # We generate files with random content.
 # head -c 1M    < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/01_origin_file_0001M.dat
 # head -c 10M   < /dev/urandom > /vagrant/experiments/01_nebula_delay/origin/02_origin_file_0010M.dat
@@ -14,23 +12,35 @@
 
 destination="192.200.1.100"
 size_of_files=(1M 10M 50M 100M 500M 1000M)
+# size_of_files=(1000M)
 #name_of_files=(ls  /vagrant/benchmark/origin/*.dat | xargs -n1 basename)
 #name_of_files=()
 name_of_files=(01_origin_file_0001M.dat 02_origin_file_0010M.dat 03_origin_file_0050M.dat 04_origin_file_0100M.dat 05_origin_file_0500M.dat 06_origin_file_1000M.dat)
+# name_of_files=(06_origin_file_1000M.dat)
 #for i in `ls /vagrant/benchmark/origin/*.dat` ;do name_of_files+=(basename $i);done
 
 #echo ${name_of_files[@]}
 
-echo "seconds;size;filename;iteration" > /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_1500_results.csv
+
 
 ## { /usr/bin/time  -f "%e, 1024" scp -q /vagrant/benchmark/origin/01_origin_file_0001M.dat vagrant@172.20.1.100:/vagrant/benchmark/destination/ ; } 2>> /vagrant/benchmark/results/delay.csv
 
+# file=0
 
 for file in {0..5}
 do
+    echo "----------------------"
+    echo "Processing ${size_of_files[$file]}; ${name_of_files[$file]}"
+    echo "----------------------"
+    echo "seconds;size;filename;iteration" > /vagrant/experiments/01_nebula_delay/results/01_result_${size_of_files[$file]}.csv
     for iteration in {1..30}
     do        
-        echo "${size_of_files[$file]}; ${name_of_files[$file]}; $iteration"
-        { /usr/bin/time  -f "%e; ${size_of_files[$file]}; ${name_of_files[$file]}; $iteration" scp -q /vagrant/experiments/01_nebula_delay/origin/${name_of_files[$file]} vagrant@$destination:/vagrant/experiments/01_nebula_delay/destination/ ; } 2>> /vagrant/experiments/01_nebula_delay/results/01_nebula_delay_1500_results.csv
+        # echo " $iteration"
+        { /usr/bin/time  -f "%e; ${size_of_files[$file]}; ${name_of_files[$file]}; $iteration" scp -q /vagrant/experiments/01_nebula_delay/origin/${name_of_files[$file]} vagrant@$destination:/vagrant/experiments/01_nebula_delay/destination/ ; } 2>> /vagrant/experiments/01_nebula_delay/results/01_result_${size_of_files[$file]}.csv
+        sleep 1
     done
+    echo "sleep 1 second"
+    sleep 1
+    echo "Go!"
 done
+
